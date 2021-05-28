0373f797e26b29e4089d888b7a99dbda372674f9
“karlo-smart” <karlo.de-guzman@smartpension.co.uk>
2021-05-28 15:29:53 +0100

    add create_movie_worker

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  require 'sidekiq/web'
-
-  Smartflix::Application.routes.draw do
-    mount Sidekiq::Web => "/sidekiq"
-  end
+  mount Sidekiq::Web => "/sidekiq"
 end
