0c467247124f79ff5f7bfe100a4824d1631281c3
karlo-smart <karlo.de-guzman@smartpension.co.uk>
2021-08-31 21:44:16 +0100

    add database cleaner config file

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.before do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+end
