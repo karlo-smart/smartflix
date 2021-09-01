66e1af78956ff40fbaff85f57e15b0d68edfcfa3
karlo-smart <karlo.de-guzman@smartpension.co.uk>
2021-09-01 08:57:34 +0100

    test movie model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(title: title) }
+
+  context 'when required attributes are present' do
+    let(:title) { 'anything' }
+
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'when required attributes are missing' do
+    let(:title) { nil }
+
+    it 'is invalid' do
+      expect(subject).to_not be_valid
+    end
+  end
 end
