19325706193cd25900ef70967e09926495a6c595
karlo-smart <karlo.de-guzman@smartpension.co.uk>
2021-09-01 08:47:45 +0100

    test unsuccessful api call

--- spec/lib/apis/omdb/client.rb
+++ spec/lib/apis/omdb/client.rb
@@ -6,22 +6,37 @@ RSpec.describe Apis::Omdb::Client do
   subject { described_class.new(title: title).get_movie }
 
   let(:api_key) { Rails.application.credentials.omdb[:api_key] }
-  let(:title) { 'Up' }
 
   before { subject }
 
   describe '#get_movie', :vcr do
-    it 'returns 200 status' do
-      expect(subject.code).to eq(200)
+    context 'when request is successful' do
+      let(:title) { 'Up' }
+
+      it 'returns 200 status' do
+        expect(subject.code).to eq(200)
+      end
+
+      it 'fetches movie data', aggregate_failures: true do
+        expect(subject['Title']).to eq(title)
+        expect(subject['Year']).to eq('2009')
+      end
+
+      it 'calls omdb endpoint' do
+        assert_requested :get, "http://www.omdbapi.com/?apikey=#{api_key}&t=#{title}", :times => 1
+      end
+
+      it 'returns True response' do
+        expect(subject['Response']).to eq('True')
+      end
     end
 
-    it 'fetches movie data', aggregate_failures: true do
-      expect(subject['Title']).to eq(title)
-      expect(subject['Year']).to eq('2009')
-    end
+    context 'when request is unsuccessful' do
+      let(:title) { 'not_a_movie_title' }
 
-    it 'calls omdb endpoint' do
-      assert_requested :get, "http://www.omdbapi.com/?apikey=#{api_key}&t=#{title}", :times => 1
+      it 'returns False response' do
+        expect(subject['Response']).to eq('False')
+      end
     end
   end
 end
