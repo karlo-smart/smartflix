05460d693e1f689f8aed7d99625f34a78fce7123
karlo-smart <karlo.de-guzman@smartpension.co.uk>
2021-09-01 08:40:55 +0100

    test endpoint and movie data from omdb api call

--- spec/lib/apis/omdb/client.rb
+++ spec/lib/apis/omdb/client.rb
@@ -3,12 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Apis::Omdb::Client do
-  subject { described_class.new(title: 'Up').get_movie }
+  subject { described_class.new(title: title).get_movie }
 
-  describe '#call', :vcr do
+  let(:api_key) { Rails.application.credentials.omdb[:api_key] }
+  let(:title) { 'Up' }
+
+  before { subject }
+
+  describe '#get_movie', :vcr do
     it 'returns 200 status' do
-      subject
       expect(subject.code).to eq(200)
+    end
+
+    it 'fetches movie data', aggregate_failures: true do
+      expect(subject['Title']).to eq(title)
+      expect(subject['Year']).to eq('2009')
+    end
+
+    it 'calls omdb endpoint' do
+      assert_requested :get, "http://www.omdbapi.com/?apikey=#{api_key}&t=#{title}", :times => 1
     end
   end
 end
