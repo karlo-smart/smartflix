0dc5fb723d3c04ae08f533d00f6080489a9f3163
karlo-smart <karlo.de-guzman@smartpension.co.uk>
2021-09-02 08:27:40 +0100

    test CreateMovieWorker

--- spec/workers/create_movie_worker_spec.rb
+++ spec/workers/create_movie_worker_spec.rb
@@ -1,4 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+
 RSpec.describe CreateMovieWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new.perform(title) }
+
+  let(:title) { 'Up' }
+  let(:movie) { instance_double('Movies::Create') }
+
+  before do
+    allow(Movies::Create).to receive(:new).and_return(movie)
+  end
+
+  it 'calls Movies::Create', aggregate_failures: true do
+    expect(Movies::Create).to receive(:new).with(title: title).and_return(movie)
+    expect(movie).to receive(:call)
+
+    subject
+  end
 end
